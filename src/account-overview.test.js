b107a04562a6e85c08ff5d21943a6f5494d3e703
rickdouglas <rickmiranda23@gmail.com>
2024-06-12 13:15:53 -0300

    feat(test): testing account overview

--- src/account-overview.test.js
+++ src/account-overview.test.js
@@ -1,8 +1,43 @@
 import { render, screen } from "@testing-library/react";
 import AccountOverview from "./account-overview";
 
-test("renders learn react link", () => {
-  render(<AccountOverview />);
-  const element = screen.getByText(/Build your components here/i);
-  expect(element).toBeInTheDocument();
+describe("AccountOverview", () => {
+  test("renders AccountOverview component", () => {
+    const mockData = {
+      supportContact: {
+        name: "John Doe",
+        email: "john.doe@example.com",
+      },
+      salesOverview: {
+        successfulUploads: 100,
+        uploads: 200,
+        linesSaved: 150,
+        linesAttempted: 300,
+        uploadPercentage: 50,
+        linesSavedPercentage: 60,
+      },
+    };
+
+    render(<AccountOverview data={mockData} />);
+
+    expect(screen.getByText("YOUR FEEFO SUPPORT CONTACT")).toBeInTheDocument();
+
+    expect(screen.getByText("Sales")).toBeInTheDocument();
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+
+    expect(screen.getByText("john.doe@example.com")).toBeInTheDocument();
+
+    expect(screen.getByText("0203 362 4209")).toBeInTheDocument();
+
+    expect(screen.getByText("Sales")).toBeInTheDocument();
+
+    expect(
+      screen.getByText("You had 100 uploads and 150 lines added")
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("UPLOAD SUCCESS")).toBeInTheDocument();
+
+    expect(screen.getByText("LINES SAVED")).toBeInTheDocument();
+  });
 });
